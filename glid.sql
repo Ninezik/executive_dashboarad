2dc158be77e85b0e6215affc1e9bc547ec096172
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-01 20:16:23 +0700

    linting
    agar pretty

--- glid.sql
+++ glid.sql
@@ -1,50 +1,70 @@
-select  t3.*,
-coalesce(UPPER(t4.ketnopen),'TIDAK TERDEFINISI') ketnopen,
-coalesce(t4.regional::varchar,'TIDAK TERDEFINISI') regional,
-coalesce(t4.kcu,'TIDAK TERDEFINISI')  kcu,
-coalesce(t4.kc,'TIDAK TERDEFINISI')  kc
-FROM
-(SELECT
-    DATE(tgl_billing) AS connote__created_at,
-    kode_nopen,
-    service_code AS connote__connote_service,
-    CASE
-        WHEN service_code = 'FFE' THEN 'EB'
-        ELSE 'WIN'
-    END AS segment,
-    COUNT(order_code) AS produksi,
-    CAST(SUM(
-        CASE 
-            WHEN LOWER(jenis_produk) LIKE '%include%'
-            THEN total_amount / (1 + 0.011)
-            ELSE total_amount 
-        END
-    ) AS DECIMAL(18,2)) AS pendapatan,
-    CAST(SUM(
-        CASE 
-            WHEN LOWER(jenis_produk) LIKE '%include%'
-            THEN total_amount - (total_amount / (1 + 0.011))
-            ELSE 0
-        END
-    ) AS DECIMAL(18,2)) AS pajak,
-    SUM(tot_weight_kg)berat,
-    'LOGISTIK' AS kelompok,
-    'GLID' AS sumber
-FROM (
 select
-	tgl_billing,
-    kode_nopen,
-    service_code,
-    order_code,
-    jenis_produk,
-    SUM(case when g.konversi_berat =g.total_qty or g.konversi_berat >=1000 then tot_weight_kg/konversi_berat
+	t3.*,
+	coalesce(UPPER(t4.ketnopen),
+	'TIDAK TERDEFINISI') ketnopen,
+	coalesce(t4.regional::varchar,
+	'TIDAK TERDEFINISI') regional,
+	coalesce(t4.kcu,
+	'TIDAK TERDEFINISI') kcu,
+	coalesce(t4.kc,
+	'TIDAK TERDEFINISI') kc
+from
+	(
+	select
+		DATE(tgl_billing) as connote__created_at,
+		kode_nopen,
+		service_code as connote__connote_service,
+		case
+			when service_code = 'FFE' then 'EB'
+			else 'WIN'
+		end as segment,
+		COUNT(order_code) as produksi,
+		cast(SUM(
+        case 
+            when LOWER(jenis_produk) like '%include%'
+            then total_amount / (1 + 0.011)
+            else total_amount 
+        end
+    ) as DECIMAL(18,
+		2)) as pendapatan,
+		cast(SUM(
+        case 
+            when LOWER(jenis_produk) like '%include%'
+            then total_amount - (total_amount / (1 + 0.011))
+            else 0
+        end
+    ) as DECIMAL(18,
+		2)) as pajak,
+		SUM(tot_weight_kg)berat,
+		'LOGISTIK' as kelompok,
+		'GLID' as sumber
+	from
+		(
+		select
+			tgl_billing,
+			kode_nopen,
+			service_code,
+			order_code,
+			jenis_produk,
+			SUM(case when g.konversi_berat = g.total_qty or g.konversi_berat >= 1000 then tot_weight_kg / konversi_berat
     else tot_weight_kg end)tot_weight_kg,
-   MAX(total_amount)total_amount
-FROM glid.glid g
-group by 1,2,3,4,5
+			MAX(total_amount)total_amount
+		from
+			glid.glid g
+		group by
+			1,
+			2,
+			3,
+			4,
+			5
 )t0
-WHERE DATE(tgl_billing) > DATE '2026-01-01'
-GROUP by 1,2,3,4)t3
+	where
+		DATE(tgl_billing) > DATE '2026-01-01'
+	group by
+		1,
+		2,
+		3,
+		4)t3
 left join
 (
 	select
@@ -78,5 +98,8 @@ left join
 on
 			t1.kdkantor = t2.nopend_dirian
 )t4
-on t3.kode_nopen=t4.kdnopen
-order by 1,2
+on
+	t3.kode_nopen = t4.kdnopen
+order by
+	1,
+	2
