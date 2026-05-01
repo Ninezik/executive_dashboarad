a3e36d2442abefb7ab106bd6bf96d05f7205995a
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-02 00:25:22 +0700

    Enhance SQL query with tax calculation and grouping
    Added calculation for tax and modified grouping in SQL query.

--- kurir_mileapp_exshopee_cod.sql
+++ kurir_mileapp_exshopee_cod.sql
@@ -15,27 +15,36 @@ from
 			UPPER(transform__channel)transform__channel,
 			np.location_data_created__custom_field__nopen,
 			UPPER(connote__connote_service) as connote__connote_service,
-			case when connote__connote_service in ('KRT','KBM','FFE','FF-LKPP') then 'LOGISTIK'
-			else 'KURIR' end as kelompok,
-		--coalesce(custom_field__final_swp,np.connote__connote_sla_day) custom_field__final_swp,
---coalesce(coalesce(date(pod__timereceive),DATE(custom_field__first_attempt_time)),DATE(connote__updated_at))pod__timereceive ,
---custom_field__is_over_sla ,
-COUNT(connote__connote_code)produksi,
-SUM(coalesce(connote__connote_service_price,0) +coalesce(connote__connote_surcharge_amount,0))pendapatan,
-SUM(coalesce(custom_field__fee_value,0)) fee_cod,
-SUM(coalesce(np.connote__chargeable_weight ,0))berat
-FROM nipos__part_2026 np 
-where UPPER(connote__location_name) != 'AGP TESTING LOCATION'
-AND UPPER(connote__connote_state) NOT IN ('CANCEL','PENDING')
-AND NOT(
-    coalesce(UPPER(customer_code) ,'')= 'DAGSHOPEE04120A'
-    AND coalesce(UPPER(custom_field__cod),'')!= 'NONCOD'
+			case
+			when connote__connote_service in ('KRT', 'KBM', 'FFE', 'FF-LKPP') then 'LOGISTIK'
+			else 'KURIR'
+		end as kelompok,
+		COUNT(connote__connote_code)produksi,
+		SUM(coalesce(connote__connote_service_price, 0) + coalesce(connote__connote_surcharge_amount, 0))pendapatan,
+		SUM(coalesce(custom_field__fee_value, 0)) fee_cod,
+		SUM(coalesce(np.connote__chargeable_weight , 0))berat,
+		SUM((coalesce(connote__connote_service_price, 0) * 0.011)+(coalesce(connote__connote_surcharge_amount, 0)* 0.11))pajak
+	from
+		nipos__part_2026 np
+	where
+		UPPER(connote__location_name) != 'AGP TESTING LOCATION'
+		and UPPER(connote__connote_state) not in ('CANCEL', 'PENDING')
+		and not(
+    coalesce(UPPER(customer_code) ,
+		'')= 'DAGSHOPEE04120A'
+			and coalesce(UPPER(custom_field__cod),
+			'')!= 'NONCOD'
 )
-AND connote__connote_amount >=0
-and np.connote__connote_service !='LNINCOMING'
-GROUP BY
-1,2,3,4,5,6)t1
---joinkeun ka referensi subdit
+		and connote__connote_amount >= 0
+		and np.connote__connote_service != 'LNINCOMING'
+	group by
+		1,
+		2,
+		3,
+		4,
+		5,
+		6)t1
+	--joinkeun ka referensi subdit
 left join
 (select distinct idregpelanggan,subdit_id
 from nipos.m_pelanggan)t2
