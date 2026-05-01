24f3bf33a16c0b208e146e65503ff21014156bbd
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-01 16:23:46 +0700

    isi nilai null
    isi dengan nilai modern channel dan tidak terdefinisi

--- jaskg_konsfila.sql
+++ jaskg_konsfila.sql
@@ -1,58 +1,84 @@
 select
-	t4.*,
-	t3.ketnopen,
-	t3.regional,
-	t3.kcu,
-	t3.kc
+		t4.tgltr,
+		t4.ketproduk ,
+		t4.kdproduk,
+		t4.kdkantor,
+		t4.produksi,
+		t4.pendapatan,
+		t4.pajak,
+		coalesce(case
+			when t4.regional_ = '00' then 'MODERN CHANNEL'
+		else t3.ketnopen ::VARCHAR
+	end,
+	'TIDAK TERDEFINISI') as ketnopen,
+	coalesce(case
+		when t4.regional_ = '00' then 'MODERN CHANNEL'
+		else t3.regional::VARCHAR
+	end,
+	'TIDAK TERDEFINISI') as regional,
+	coalesce(case
+		when t4.regional_ = '00' then 'MODERN CHANNEL'
+		else t3.kcu
+	end,
+	'TIDAK TERDEFINISI') as kcu,
+	coalesce(case
+		when t4.regional_ = '00' then 'MODERN CHANNEL'
+		else t3.kc
+	end,
+	'TIDAK TERDEFINISI') as kc
 from
-	(
+		(
 	select
-		date(tgltr)tgltr,
-		ketproduk ,
-		kdproduk ,
-		kdkantor,
-		SUM(produksi)produksi,
-		SUM(pendapatan)pendapatan,
-		SUM(pajak)pajak
+			date(tgltr)tgltr,
+			ketproduk ,
+			kdproduk ,
+			kdkantor,
+			regional regional_,
+			SUM(produksi)produksi,
+			SUM(pendapatan)pendapatan,
+			SUM(pajak)pajak
 	from
-		sap.feeder_sap
+			sap.feeder_sap
 	group by
-		1,
-		2,
-		3,
-		4
+			1,
+			2,
+			3,
+			4,
+			5
 )t4
 	--	joinkeun ka referensi kantorexcept 
 left join
 (
 	select
-		distinct t1.kdnopen,
-		t1.ketnopen,
-		t2.regional,
-		t2.kcu,
-		t2.kc
+			distinct 
+			t1.kdnopen,
+			t1.ketnopen,
+			t2.regional,
+			t2.kcu,
+			t2.kc
 	from
-		(
+			(
 		select
-			kdnopen,
-			ketnopen,
-			kdkantor
+				kdnopen,
+				ketnopen,
+				kdkantor
 		from
-			referensi.refrensikantorbaru
+				referensi.refrensikantorbaru
 )t1
 		--referensi_lengkap
 	join
 (
 		select
 			distinct 
-		nopend_dirian,
-			kc,
-			kcu,
-			regional
+			nopend_dirian,
+				kc,
+				kcu,
+				regional
 		from
-			referensi.ref_kcu_kc_2023
+				referensi.ref_kcu_kc_2023
 )t2
 on
-		t1.kdkantor = t2.nopend_dirian
+			t1.kdkantor = t2.nopend_dirian
 )t3
-on t4.kdkantor =t3.kdnopen 
+on
+		t4.kdkantor = t3.kdnopen
