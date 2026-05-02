0a3526cd59bafffccbdbf8244b1a6857a1e5eac1
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-03 02:11:29 +0700

    Update glid.sql

--- glid.sql
+++ glid.sql
@@ -9,15 +9,15 @@ select
 	coalesce(t4.kc,
 	'TIDAK TERDEFINISI') kc
 from
-	(
-	select
+	(select
 		DATE(tgl_billing) as connote__created_at,
-		kode_nopen,
+		null pod__timereceive ,
+		'GLID' status_sla,
+		'GLID' customer_code,
+		'GLID' transform__channel,
+		location_data_created__custom_field__nopen,
 		service_code as connote__connote_service,
-		case
-			when service_code = 'FFE' then 'EB'
-			else 'WIN'
-		end as segment,
+		'LOGISTIK' as kelompok,
 		COUNT(order_code) as produksi,
 		cast(SUM(
         case 
@@ -27,6 +27,8 @@ from
         end
     ) as DECIMAL(18,
 		2)) as pendapatan,
+		0 fee_cod,
+		SUM(tot_weight_kg)berat,
 		cast(SUM(
         case 
             when LOWER(jenis_produk) like '%include%'
@@ -34,36 +36,29 @@ from
             else 0
         end
     ) as DECIMAL(18,2)) as pajak,
-		SUM(tot_weight_kg)berat,
-		'LOGISTIK' as kelompok,
+    subdit_id,
 		'GLID' as sumber
 	from
 		(
 		select
 			tgl_billing,
-			kode_nopen,
+			kode_nopen location_data_created__custom_field__nopen,
 			service_code,
 			order_code,
 			jenis_produk,
+			case
+			when service_code = 'FFE' then 'EB'
+			else 'WIN'
+		end as subdit_id,
 			SUM(case when g.konversi_berat = g.total_qty or (g.konversi_berat >= 1000 and total_qty>1) then tot_weight_kg / konversi_berat
     else tot_weight_kg end)tot_weight_kg,
 			MAX(total_amount)total_amount
 		from
 			glid.glid g
-		group by
-			1,
-			2,
-			3,
-			4,
-			5
+		where DATE(tgl_billing) >='20260101'
+		group by 1,2,3,4,5,6
 )t0
-	where
-		DATE(tgl_billing) > DATE '2026-01-01'
-	group by
-		1,
-		2,
-		3,
-		4)t3
+group by 1,2,3,4,5,6,7,8,t0.subdit_id)t3
 left join
 (
 	select
@@ -98,7 +93,7 @@ on
 			t1.kdkantor = t2.nopend_dirian
 )t4
 on
-	t3.kode_nopen = t4.kdnopen
+	t3.location_data_created__custom_field__nopen  = t4.kdnopen
 order by
 	1,
 	2
