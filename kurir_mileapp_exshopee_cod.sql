4d6458b142a4256cc0c5cf66c50c880d248bef93
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-04 23:42:01 +0700

    Fix SQL query to use correct field references

--- kurir_mileapp_exshopee_cod.sql
+++ kurir_mileapp_exshopee_cod.sql
@@ -24,19 +24,19 @@ from
 	(
 	select
 		DATE(connote__created_at) as connote__created_at,
-		DATE(np.pod__timereceive)pod__timereceive ,
+		DATE(pod__timereceive)pod__timereceive ,
 		--		cek data swp
 case
-			when np.custom_field__final_swp is null then 'NILAI SWP TIDAK TERDEFINISI'
+			when custom_field__final_swp is null then 'NILAI SWP TIDAK TERDEFINISI'
 			--		cek status swp
 			--		ketika pod ada
-			when np.pod__timereceive is not null
-			and date(np.pod__timereceive)<= DATE(connote__created_at)+ np.custom_field__final_swp then 'ON TIME'
+			when pod__timereceive is not null
+			and date(pod__timereceive)<= DATE(connote__created_at)+ custom_field__final_swp then 'ON TIME'
 			--		ketika pod tidak ada
 			else
 (
 case
-				when date(np.connote__created_at) = CURRENT_DATE
+				when date(connote__created_at) = CURRENT_DATE
 				or CURRENT_DATE <= date(connote__created_at)+ custom_field__final_swp
 then 'ON PROCESS'
 				else 'LATE'
@@ -45,9 +45,8 @@ then 'ON PROCESS'
 		end as status_sla,
 		UPPER(customer_code)customer_code,
 		UPPER(transform__channel)transform__channel,
-		np.location_data_created__custom_field__nopen,
+		location_data_created__custom_field__nopen,
 		UPPER(connote__connote_service) as connote__connote_service,
-		'MENUNGGU DATA REFERENSI' nama_produk,
 		case
 			when connote__connote_service in ('KRT', 'KBM', 'FFE', 'FF-LKPP') then 'LOGISTIK'
 			else 'KURIR'
@@ -60,10 +59,10 @@ then 'ON PROCESS'
 		COUNT(connote__connote_code)produksi,
 		SUM(coalesce(connote__connote_service_price, 0) + coalesce(connote__connote_surcharge_amount, 0))pendapatan,
 		SUM(coalesce(custom_field__fee_value, 0)) fee_cod,
-		SUM(coalesce(np.connote__chargeable_weight , 0))berat,
+		SUM(coalesce(connote__chargeable_weight , 0))berat,
 		SUM((coalesce(connote__connote_service_price, 0) * 0.011)+(coalesce(connote__connote_surcharge_amount, 0)* 0.11))pajak
 	from
-		nipos__part_2026 np
+		nipos__part_2026
 	where
 		UPPER(connote__location_name) != 'AGP TESTING LOCATION'
 		and UPPER(connote__connote_state) not in ('CANCEL', 'PENDING')
@@ -72,7 +71,7 @@ then 'ON PROCESS'
 			and coalesce(UPPER(custom_field__cod),
 			'')!= 'NONCOD')
 		and connote__connote_amount >= 0
-		and np.connote__connote_service != 'LNINCOMING'
+		and connote__connote_service != 'LNINCOMING'
 	group by
 		1,
 		2,
@@ -82,8 +81,8 @@ then 'ON PROCESS'
 		6,
 		7,
 		8,
-		9,
-		10)t1
+		9
+		limit 10)t1
 	--joinkeun ka referensi subdit
 left join
 (
