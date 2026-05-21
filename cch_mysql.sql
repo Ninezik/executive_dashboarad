91aa1d7eb90be28d8e7c3357b4ad688f7e8b8296
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-21 21:24:06 +0700

    Update cch_mysql.sql

--- cch_mysql.sql
+++ cch_mysql.sql
@@ -1,32 +1,33 @@
-WITH sumber AS (
-    SELECT 
-    date(tanggal_tambah)tanggal_tambah,
-    date(tanggal_status)tanggal_status,
-    Sumber_Pengaduan ,
-    jenis_pengaduan,
-        semua_tujuan,
-        status_akhir,
-        COUNT(*) AS nilai
-    FROM cchentri
-    where tanggal_tambah>'20260507'
-and tanggal_tambah<'20260508'
-    GROUP BY 1,2,3,4,5,6
-)
 SELECT 
-s.tanggal_tambah ,
-s.tanggal_status ,
-s.sumber_pengaduan ,
-s.jenis_pengaduan ,
-    jt.nopen,
-    s.status_akhir,
-    SUM(s.nilai) AS total_nilai,
-    SUM(sum(s.nilai)) OVER() total_seluruh
-FROM sumber s
-CROSS JOIN JSON_TABLE(
-    CONCAT('["', REPLACE(s.semua_tujuan, ',', '","'), '"]'),
-    '$[*]' COLUMNS (
-        nopen VARCHAR(20) PATH '$'
-    )
-) jt
-GROUP BY 1,2,3,4,5,6
-ORDER BY total_nilai desc
+t1.kantor_tujuan_update ,
+date(cchentri.Tanggal_Tambah)Tanggal_Tambah ,
+date(cchentri.Tanggal_Status)Tanggal_Status ,
+cch_jenis_pengaduan.Deskripsi jenis_pengaduan,
+cch_sumber_pengaduan.Sumber sumber_pengaduan,
+cch_jenis_layanan.Deskripsi jenis_layanan,
+cch_jenis_penanganan.Deskripsi_Status ,
+COUNT(*)jumlah_pengaduan,
+SUM(COUNT(*))OVER() uji_total
+FROM (
+    -- Langkah 1: Ambil daftar kantor unik yang statusnya '101'
+    SELECT DISTINCT cchentridet.Kantor_Tujuan_Update
+    FROM cchentridet 
+    JOIN cchentri 
+    ON cchentri.ID_Pengaduan = cchentridet.ID_Pengaduan
+    WHERE cchentridet.Status_Update = '101'
+    AND cchentri.Tanggal_Tambah >'20260506'
+    and cchentri.Tanggal_Tambah <'20260507'
+) AS t1
+JOIN cchentri 
+ON cchentri.Semua_Tujuan LIKE CONCAT('%', t1.Kantor_Tujuan_Update, '%')
+left join cch_sumber_pengaduan
+on cchentri.Sumber_Pengaduan =cch_sumber_pengaduan.id
+left join cch_jenis_pengaduan
+on cchentri.Jenis_Pengaduan =cch_jenis_pengaduan.Kode_Jenis
+left join cch_jenis_layanan
+on cchentri.Jenis_Kiriman =cch_jenis_layanan.Kode_Layanan 
+left join cch_jenis_penanganan
+on cchentri.Status_Akhir =cch_jenis_penanganan.ID_Status
+WHERE cchentri.Tanggal_Tambah >'20260506'
+and cchentri.Tanggal_Tambah <'20260507'
+group by 1,2,3,4,5,6,7
