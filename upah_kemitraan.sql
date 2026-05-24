e1a341cc4b9356364b7251925f4e713a77e885c3
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-24 13:42:31 +0700

    ambil kantor kcp lpu dari t_mitra lpu
    khussu mitra lpu kcp ambi dari kantor daftar kcp lpu

--- upah_kemitraan.sql
+++ upah_kemitraan.sql
@@ -22,14 +22,19 @@ FROM (
     FROM t_upah
     WHERE LEFT(t_upah.bulan_transaksi, 4)>'2025'
     UNION ALL
-    SELECT 
-        nopend,
-        CONVERT(date, LEFT(bulan_transaksi, 7) + '-01') AS bulan_transaksi,
-        id_mitra,
-        total_kolekting_antaran AS total_fee,
-        produksi
-    FROM t_upah_lpu
-    WHERE LEFT(t_upah_lpu.bulan_transaksi, 4)>'2025'
+--    mitra lpu
+SELECT
+	coalesce(t_mitra.kantor,t_upah_lpu.nopend) nopend,
+	CONVERT(date,LEFT(t_upah_lpu.bulan_transaksi,7) + '-01') AS bulan_transaksi,
+	t_upah_lpu.id_mitra,
+	t_upah_lpu.total_kolekting_antaran total_fee,
+	t_upah_lpu.produksi
+FROM
+	t_upah_lpu
+LEFT JOIN t_mitra
+	ON
+	t_upah_lpu.id_mitra = t_mitra.id_mitra
+	WHERE left(bulan_transaksi,4)>'2025'
 ) a
 LEFT JOIN ref_jbt
     ON LEFT(a.id_mitra, 3) = ref_jbt.id_regmitra
