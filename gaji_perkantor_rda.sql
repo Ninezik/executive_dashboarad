11e57c316d33289ed6632ab76b0c4d5885787b13
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-26 16:04:06 +0700

    Create gaji_perkantor_rda.sql

---
+++ gaji_perkantor_rda.sql
@@ -0,0 +1,62 @@
+select tfinal.deskripsi,
+date_trunc('month',tfinal.tgltransaksi )tgltransaksi,
+tfinal.kpc,
+coalesce(case when tfinal.kpc='40005' then 'KANTOR PUSAT BANDUNG'
+else tfinal.regional  end,'TIDAK TERDEFINISI') as regional,
+coalesce(case when tfinal.kpc='40005' then 'KANTOR PUSAT BANDUNG'
+else tfinal.kcu  end,'TIDAK TERDEFINISI') as kcu,
+coalesce(case when tfinal.kpc='40005' then 'KANTOR PUSAT BANDUNG'
+else tfinal.kc  end,'TIDAK TERDEFINISI') as kc,
+coalesce(tfinal.ketnopen,'TIDAK TERDEFINISI')ketnopen,
+SUM(tfinal.amount)amount
+FROM
+(SELECT
+    bp.*,
+    ref.ketnopen,
+    ref.regional,
+    ref.kcu,
+    ref.kc,
+    ref.jenis
+FROM public."Biaya_Pegawai" bp
+LEFT JOIN
+(
+    SELECT *
+    FROM
+    (
+        SELECT
+            t1.kdnopen,
+            t1.ketnopen,
+            t2.regional,
+            t2.kcu,
+            t2.kc,
+            t1.jenis,
+            ROW_NUMBER() OVER (
+                PARTITION BY t1.kdnopen
+                ORDER BY t1.ketnopen
+            ) AS rn
+        FROM
+        (
+            SELECT
+                "KDNOPEN"  AS kdnopen,
+                UPPER("KETNOPEN") AS ketnopen,
+                "KDKANTOR" AS kdkantor,
+                "JENIS"    AS jenis
+            FROM public."REFRENSIKANTORBARU"
+        ) t1
+        LEFT JOIN
+        (
+            SELECT DISTINCT
+                "NOPEND_DIRIAN" AS nopend_dirian,
+                "KC" AS kc,
+                "KCU" AS kcu,
+                "REGIONAL" AS regional
+            FROM public."ref_kcu_kc_2023"
+        ) t2
+            ON t1.kdkantor = t2.nopend_dirian
+    ) a
+    WHERE rn = 1
+) ref
+    ON bp.kpc = ref.kdnopen
+)tfinal
+--where tfinal.kpc='40005'
+group by 1,2,3,4,5,6,7
