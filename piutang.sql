58dd09f6ee753e6f0d9a5925422abba4148669ce
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-07-21 10:18:42 +0700

    Refactor SQL query for better readability and structure

--- piutang.sql
+++ piutang.sql
@@ -1,70 +1,68 @@
-select 
-sp.gl_account_number ,
-sp.gl_account_text,
-sp.external_customer_no ,
-t4.regional,
-t4.kcu,
-t4.kc,
-t4.ketnopen kcp,
-t4.kdnopen,
-t4.jenis,
-coalesce(tsubdit.subdit_id,'TIDAK TERDEFINISI'),
-SUM(sp.amount)nilai
---SUM(SUM(amount))OVER() total
-from sap.sap_piutang sp 
-left join
-(
-	select
-		*
-	from
-		(
-		select
-			t1.kdnopen,
-			UPPER(t1.ketnopen) ketnopen,
-			t2.regional,
-			t2.kcu,
-			t2.kc,
-			t1.jenis,
-			row_number() over (partition by t1.kdnopen
-		order by
-			t1.kdnopen) as rn
-		from
-			(
-			select
-				kdnopen,
-				ketnopen,
-				kdkantor,
-				jenis
-			from
-				referensi.refrensikantorbaru
-) t1
-		join (
-			select
-				distinct
-nopend_dirian,
-				kc,
-				kcu,
-				regional
-			from
-				referensi.ref_kcu_kc_2023
-) t2
-on
-			t1.kdkantor = t2.nopend_dirian
-) x
-	where
-		rn = 1
-)t4
-on
-	SUBSTRING(sp.profit_center,4,5) = t4.kdnopen
-
---subdit
-left join
-(
-	select
-		distinct idregpelanggan,
-		subdit_id
-	from
-		referensi.m_pelanggan)tsubdit
-on
-	sp.external_customer_no= tsubdit.idregpelanggan
-group by 1,2,3,4,5,6,7,8,9,10
+SELECT 
+    sp.gl_account_number,
+    sp.gl_account_text,
+    sp.external_customer_no,
+    t4.regional,
+    t4.kcu,
+    t4.kc,
+    t4.ketnopen AS kcp,
+    t4.kdnopen,
+    t4.jenis,
+    COALESCE(tsubdit.subdit_id, 'TIDAK TERDEFINISI') AS subdit_id,
+    SUM(sp.amount) AS nilai,
+    SUM(SUM(sp.amount)) OVER () AS total
+FROM sap.sap_piutang sp
+LEFT JOIN (
+    SELECT *
+    FROM (
+        SELECT
+            t1.kdnopen,
+            UPPER(t1.ketnopen) AS ketnopen,
+            t2.regional,
+            t2.kcu,
+            t2.kc,
+            t1.jenis,
+            ROW_NUMBER() OVER (
+                PARTITION BY t1.kdnopen
+                ORDER BY t1.kdnopen
+            ) AS rn
+        FROM (
+            SELECT
+                kdnopen,
+                ketnopen,
+                kdkantor,
+                jenis
+            FROM referensi.refrensikantorbaru
+        ) t1
+        JOIN (
+            SELECT DISTINCT
+                nopend_dirian,
+                kc,
+                kcu,
+                regional
+            FROM referensi.ref_kcu_kc_2023
+        ) t2
+            ON t1.kdkantor = t2.nopend_dirian
+    ) x
+    WHERE rn = 1
+) t4
+    ON SUBSTRING(sp.profit_center, 4, 5) = t4.kdnopen
+LEFT JOIN (
+    SELECT DISTINCT
+        idregpelanggan,
+        subdit_id
+    FROM referensi.m_pelanggan
+) tsubdit
+    ON sp.external_customer_no = tsubdit.idregpelanggan
+WHERE sp.gl_account_number <> '2101010001'
+GROUP BY
+    sp.gl_account_number,
+    sp.gl_account_text,
+    sp.external_customer_no,
+    t4.regional,
+    t4.kcu,
+    t4.kc,
+    t4.ketnopen,
+    t4.kdnopen,
+    t4.jenis,
+    COALESCE(tsubdit.subdit_id, 'TIDAK TERDEFINISI');
