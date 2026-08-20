adc5c2cc6b33c49202d6f790329d8905e3daae38
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-08-20 10:38:16 +0700

    Update kargo_haji_2025_26.sql

--- kargo_haji_2025_26.sql
+++ kargo_haji_2025_26.sql
@@ -1,12 +1,20 @@
-SELECT date(created_at)created_at ,
-COUNT(distinct no_resi)trx,
-SUM(total_fee_idr)/(1+(1.1/100)) pendapatan,
-SUM(total_fee_idr)-(SUM(total_fee_idr)/(1+(1.1/100))) pajak,
+SELECT date(created_at)connote__created_at,
+'KARGO HAJI' customer_code,
+'KARGO HAJI' custom_field__jenis_barang,
+'KARGO HAJI' location_data_created__custom_field__nokprk,
+'KARGO HAJI' transform__channel,
+'KARGO HAJI' connote__connote_service,
+NULL connote_sender_custom_field__pks_no__to_be_verified,
+SUM(coalesce(total_fee_idr,0))connote__connote_amount,
+COUNT(distinct no_resi)connote__connote_code,
+SUM(coalesce(total_fee_idr,0))/(1+(1.1/100)) pendapatan,
+SUM(coalesce(total_fee_idr,0))-(SUM(coalesce(total_fee_idr,0))/(1+(1.1/100))) pajak,
+0 fee_cod,
 'KARGO HAJI' sumber,
-'KURIR' kelompok,
-'WIN' subdit_id
+SUM(coalesce(total_weight,0))connote__chargeable_weight
 FROM kargo.kargo_haji_kolekting
 where UPPER(status)!='CANCEL'
-and kargo_haji_kolekting.created_at >'20260101'
 and is_paid='t'
-GROUP by 1
+and created_at>DATE_TRUNC('year', CURRENT_DATE)
+GROUP BY
+1,2,3,4,5,6,7
